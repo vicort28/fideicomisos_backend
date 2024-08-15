038667db888674373205d61d5e1898102f129131
Heber González <gonzalez_heber@outlook.com>
2024-08-15 14:05:41 -0600

    docker ready

--- fideicomisos/settings.py
+++ fideicomisos/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5nc$bz9+cl*37!jrxgpsfi#@-=6uv5o32n$uqz&&bb*yp-3b1)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -111,12 +116,25 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+print(env('SERVER_DB'))
+
+DATABASES = { 
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': env('SERVER_DB'), 
+         'USER': 'developer',
+         'PASSWORD': '$Ultra64#.', 
+         'HOST': env('SERVER_ADDRESS'),
+         'PORT': '3306', 
+     } 
+ } 
 
 
 # Password validation
